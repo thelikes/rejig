803b5caf15da7e9cc3b52e4c8c0d32dd86e16125
thelikes <thelikes@github.com>
2020-08-16 18:06:56 -0700

    update tool list generator

--- ansible/update-tool-list.sh
+++ ansible/update-tool-list.sh
@@ -4,18 +4,18 @@ echo
 echo "### Core packages"
 echo
 
-grep '      name: ' roles/corepkgs/tasks/main.yml | cut -d\: -f2- | sed 's/^/    -/g'
+grep '      name: ' roles/corepkgs/tasks/main.yml | cut -d\: -f2- | sed 's/^/-/g'
 
 echo
 echo "### Go tools"
 echo
 
-grep '      url: ' roles/go-get/vars/main.yml | grep -v '#' | cut -d\: -f2- | sed 's/^/    -/g'
+grep '      url: ' roles/go-get/vars/main.yml | grep -v '#' | cut -d\: -f2- | sed 's/^/-/g'
 
 echo
 echo "### Github repos"
 echo
 
-grep '      url:' roles/pop-opt/vars/main.yml | cut -d\: -f2- | grep -v '{' | sed 's/^/    -/g' | sed 's/https:\/\///g'
+grep '      url:' roles/pop-opt/vars/main.yml | cut -d\: -f2- | grep -v '{' | sed 's/^/-/g' | sed 's/https:\/\///g'
 
 
