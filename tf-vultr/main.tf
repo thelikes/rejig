0fe3a534536533d8592dedd05dd99f1ec3b53ae1
thelikes <thelikes@github.com>
2020-10-05 18:14:27 -0700

    add proxy/vpn box to vultr

--- tf-vultr/main.tf
+++ tf-vultr/main.tf
@@ -4,9 +4,11 @@ resource "vultr_server" "strike" {
     os_id = "387"
     tag = "atk"
     hostname = var.strike_img_name
+    label = var.strike_img_name
     enable_ipv6 = true
     auto_backup = false
     ssh_key_ids = var.vultr_ssh_key_id
+    count = 1
 
     provisioner "remote-exec" {
   
@@ -27,4 +29,37 @@ resource "vultr_server" "strike" {
 
 output "Strike_PublicIP" {
     value = vultr_server.strike[*].main_ip
+}
+
+resource "vultr_server" "proxy" {
+    plan_id = "201"
+    region_id = "1"
+    os_id = "387"
+    tag = "proxy"
+    hostname = var.proxy_img_name
+    label = var.proxy_img_name
+    enable_ipv6 = true
+    auto_backup = false
+    ssh_key_ids = var.vultr_ssh_key_id
+    count = 1
+
+    provisioner "remote-exec" {
+  
+        inline = ["apt update && apt install python -y"]
+
+        connection {
+            type = "ssh"
+            user = "root"
+            private_key = "${file(var.vultr_key_path)}"
+            host = self.main_ip
+        }
+    }
+
+    provisioner "local-exec" {
+        command = "ansible-playbook -u root -i '${self.main_ip}', --private-key ${var.vultr_key_path} ../ansible/playbook-foundation.yml --vault-password-file ${var.my_vualt_file}"
+    }
+}
+
+output "Proxy_PublicIP" {
+    value = vultr_server.proxy[*].main_ip
 }
--- tf-vultr/variables.tf
+++ tf-vultr/variables.tf
@@ -1,3 +1,6 @@
 variable "strike_img_name" {
     default = "strike"
 }
+variable "proxy_img_name" {
+    default = "proxy"
+}
