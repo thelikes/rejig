e9956d4020820da7909728a55e3d1e58649a4eec
thelikes <thelikes@github.com>
2021-02-08 17:41:21 -0800

    update wpscan install

--- ansible/roles/wpscan-install/tasks/install-wpscan.sh
+++ ansible/roles/wpscan-install/tasks/install-wpscan.sh
@@ -1,23 +1,8 @@
-#!/bin/bash
+apt install git curl autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev
 
-# setup
-apt install gnupg2
-gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-installer | bash
 
-# install rvm
-cd /tmp
-curl -sSL https://get.rvm.io -o rvm.sh
-cat /tmp/rvm.sh | bash -s stable --rails
-source /usr/local/rvm/scripts/rvm
-export PATH=$PATH:/usr/local/rvm/bin/
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.zshrc
+echo 'eval "$(rbenv init -)"' >> ~/.zshrc
 
-# use rvm
-rvm install 2.6
-rvm use 2.6
-
-# install nokogiri
-apt-get install build-essential patch ruby-dev zlib1g-dev liblzma-dev -y
-gem install nokogiri
-
-# wpscan
-gem install wpscan
+/root/.rbenv/versions/3.0.0/bin/gem install wpscan
